0c865e04cf044819c529ad17f6daa791342b70e3
mplicinski <mplicinski@gmail.com>
2025-01-23 21:29:22 -0600

    lazy loading tutorials so reactive events don't start until tutorial is selected

--- src/pages/TutorialPage.tsx
+++ src/pages/TutorialPage.tsx
@@ -1,85 +1,94 @@
-import { createSignal, For } from 'solid-js';
-
-import SimpleSignalsEaxmple from '@components/tutorials/SimpleSignalsExample';
-import BasicFormExample from '@components/tutorials/BasicFormExample';
-import EventHandlersExample from '@components/tutorials/EventHandlersExample';
-import SimpleStoresExample from '@components/tutorials/SimpleStoresExample';
-import DataFetchingExample from '@components/tutorials/DataFetchingExample';
-import SimpleEffectsExample from '@components/tutorials/SimpleEffectsExample';
-import SimpleEffectsExample2 from '@components/tutorials/SimpleEffectsExample2';
-import ContextExample from '@components/tutorials/ContextExample';
+import { createSignal, For, lazy } from 'solid-js';
 
 import Button from '@components/ui/Button';
 
 const SolidConceptsExamplesPage = () => {
   const [currentPage, setCurrentPage] = createSignal('');
 
+  // lazy load components so that reactive events only start when user selects the tutorial
   const tutorials = [
     {
       id: 'simpleSignalsExample',
-      label: 'Simple Signals Exmaple',
-      component: <SimpleSignalsEaxmple />,
+      label: 'Simple Signals Example',
+      component: lazy(
+        () => import('@components/tutorials/SimpleSignalsExample'),
+      ),
     },
     {
       id: 'basicFormExample',
       label: 'Basic Form Example',
-      component: <BasicFormExample />,
+      component: lazy(() => import('@components/tutorials/BasicFormExample')),
     },
     {
       id: 'eventHandlersExample',
       label: 'Event Handlers Example',
-      component: <EventHandlersExample />,
+      component: lazy(
+        () => import('@components/tutorials/EventHandlersExample'),
+      ),
     },
     {
       id: 'simpleStoresExample',
       label: 'Simple Stores Example',
-      component: <SimpleStoresExample />,
+      component: lazy(
+        () => import('@components/tutorials/SimpleStoresExample'),
+      ),
     },
     {
       id: 'simpleEffectsExample',
       label: 'Simple Effects Example',
-      component: <SimpleEffectsExample />,
+      component: lazy(
+        () => import('@components/tutorials/SimpleEffectsExample'),
+      ),
     },
     {
       id: 'simpleEffectsExample2',
       label: 'Simple Effects Example 2',
-      component: <SimpleEffectsExample2 />,
+      component: lazy(
+        () => import('@components/tutorials/SimpleEffectsExample2'),
+      ),
     },
     {
       id: 'contextExample',
       label: 'Context Example',
-      component: <ContextExample />,
+      component: lazy(() => import('@components/tutorials/ContextExample')),
     },
     {
       id: 'dataFetchingExample',
       label: 'Data Fetching Example',
-      component: <DataFetchingExample />,
+      component: lazy(
+        () => import('@components/tutorials/DataFetchingExample'),
+      ),
     },
   ];
 
-  const renderComponent = () =>
-    tutorials.find((tutorial) => tutorial.id === currentPage())?.component || (
-      <div class="text-4xl font-bold">Select a Tutorial</div>
+  const renderComponent = () => {
+    const selectedTutorial = tutorials.find(
+      (tutorial) => tutorial.id === currentPage(),
     );
+    if (selectedTutorial?.component) {
+      const Component = selectedTutorial.component; // call lazy-loaded component
+      return <Component />; // must be returned as TSX element
+    }
+    return <div class="text-4xl font-bold">Select a Tutorial</div>;
+  };
 
   return (
     <>
       <div class="">
-        <div class="">
-          <div class="mx-auto mt-4 grid grid-cols-2 gap-4">
-            <For each={tutorials}>
-              {(tutorial) => (
-                <Button
-                  extraClasses="mx-auto w-64"
-                  onClick={() => setCurrentPage(tutorial.id)}
-                >
-                  {tutorial.label}
-                </Button>
-              )}
-            </For>
-          </div>
-          <div></div>
+        <div class="mx-auto mt-4 grid grid-cols-2 gap-4">
+          <For each={tutorials}>
+            {(tutorial) => (
+              <Button
+                extraClasses="mx-auto w-64"
+                onClick={() => setCurrentPage(tutorial.id)}
+              >
+                {tutorial.label}
+              </Button>
+            )}
+          </For>
         </div>
+
+        {/* tutorial section */}
         <div class="mx-auto mt-8 w-max">{renderComponent()}</div>
       </div>
     </>
